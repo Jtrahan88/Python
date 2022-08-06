d4be3506b00377bc2e335c4740d3502575033a32
Jacque Trahan <69168405+Jtrahan88@users.noreply.github.com>
2022-08-06 17:26:00 -0500

    Python-Fundamentals

--- 01. Basic Syntax, Conditional Statements and Loops/2_Project_Tip_Calculator.py
+++ 01. Basic Syntax, Conditional Statements and Loops/2_Project_Tip_Calculator.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator.")
 
-Bill = float(input("What was the total bill?: "))
+Bill = float(input("What was the total bill?: $"))
 tipAmount = int(input("What percentage(%) tip would you like to give? 10, 12, 15, enter other number amount "))
 totalPeople = int(input("How many people to split the bill? "))
 
@@ -14,5 +14,5 @@ totalPeople = int(input("How many people to split the bill? "))
 # all the above in one line
 totalBill = round((Bill * (tipAmount/100) + Bill) / totalPeople,2)
 
-
-print(f"Each person should pay: ${totalBill}")
+# needed to us :.2f to be sure to always get 2 decimal places not just one. 
+print("Each person should pay: ${:.2f}".format(totalBill))
