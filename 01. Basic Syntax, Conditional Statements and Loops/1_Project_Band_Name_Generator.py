95e19a27dd263fe4a8288d4f6632d19102f71266
Jacque Trahan <69168405+Jtrahan88@users.noreply.github.com>
2022-08-08 20:06:32 -0500

    Python-Fundamentals

--- 01. Basic Syntax, Conditional Statements and Loops/1_Project_Band_Name_Generator.py
+++ 01. Basic Syntax, Conditional Statements and Loops/1_Project_Band_Name_Generator.py
@@ -1,3 +1,4 @@
 city = str(input('What city did you grow up in? \n')).title()
 pet = str(input('What is the name of your pet? \n')).title()
 print(f'Your band name is now {city}{pet}'.format(city=city, pet=pet))
+
