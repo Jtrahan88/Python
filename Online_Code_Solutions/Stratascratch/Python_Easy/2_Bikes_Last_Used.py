645589ada503e98daf6947e7991724f08d3b2f4f
Jacque Trahan <69168405+Jtrahan88@users.noreply.github.com>
2022-08-22 16:27:17 -0500

    Python-Medium

---
+++ Online_Code_Solutions/Stratascratch/Python_Easy/2_Bikes_Last_Used.py
@@ -0,0 +1,8 @@
+# OUTPUTs: bike Num, datestamp
+# find last time bikes were used, 
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+dc_bikeshare_q1_2012.groupby(['bike_number'])['end_time'].max().reset_index()
