3abb592c91106ec6ed4252e181b4357fec4f54dd
Jacque Trahan <69168405+Jtrahan88@users.noreply.github.com>
2022-08-22 18:14:43 -0500

    Python-Easy

---
+++ Online_Code_Solutions/Stratascratch/Python_Easy/3_Count_nominated.py
@@ -0,0 +1,8 @@
+# OUTPUTs: num movies abigail Breslin was nominated for an oscar
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+oscar_nominees = oscar_nominees[oscar_nominees['nominee'] == 'Abigail Breslin']
+oscar_nominees['nominee'].count()
