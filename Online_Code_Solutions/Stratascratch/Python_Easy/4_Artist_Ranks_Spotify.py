279c2cc67885aa13cac2735cb6c2e5b8b5a978ef
Jacque Trahan <69168405+Jtrahan88@users.noreply.github.com>
2022-08-22 20:23:24 -0500

    Python-Easy

---
+++ Online_Code_Solutions/Stratascratch/Python_Easy/4_Artist_Ranks_Spotify.py
@@ -0,0 +1,10 @@
+# OUTPUTs: artist name, num of occurances
+# count number of times artist was on spotify's ranking list
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+counts = spotify_worldwide_daily_song_ranking.groupby(['artist'])['id'].count().reset_index().rename(columns={'id':'times_ranked'})
+
+counts.sort_values(by='times_ranked', ascending=False)
