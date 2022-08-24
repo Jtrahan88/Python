7c33162f3ccb61e94ec0cfedc8f320e58b2d3c86
Jacque Trahan <69168405+Jtrahan88@users.noreply.github.com>
2022-08-23 19:20:47 -0500

    Python-Basics

---
+++ Online_Code_Solutions/HackerRank/Python-Basics/3_Arithmetic_Operators.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    #The first line contains the sum of the two numbers.
+    #The second line contains the difference of the two numbers (first - second).
+    #The third line contains the product of the two numbers.
+    
+    print(a+b)
+    print(a-b)
+    print(a*b)
