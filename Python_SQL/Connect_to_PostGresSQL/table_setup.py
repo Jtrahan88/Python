a5a7307ff45f37c04e361c30fd437a874174cfee
Jacque Trahan <69168405+Jtrahan88@users.noreply.github.com>
2022-11-23 21:01:13 -0600

    Set up table names
    Added a try and except clause.

--- Python_SQL/Connect_to_PostGresSQL/table_setup.py
+++ Python_SQL/Connect_to_PostGresSQL/table_setup.py
@@ -1,6 +1,11 @@
 def table_setup():
     '''Get: Number Of Columns, and Each Column Name(case sensative)'''
-    userNumOfColumns = int(input('# of columns in table: '))
+    try:
+        userNumOfColumns = abs(int(input('# of columns in table: ')))
+    except ValueError:
+        print("Intergers only!")
+        table_setup()
+        
     col_nums = 1
     columns = []
     while col_nums <= userNumOfColumns:
